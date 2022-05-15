4c64839e6d2c923f7f9288dc69728e96da0ab25b
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2022-05-15 18:31:56 -0400

    Added database connection and installed dependencies

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -8,10 +8,24 @@ const path = require('path');
 const axios = require('axios');
 const morgan = require('morgan');
 
+// database
+const mongoose = require('mongoose');
+mongoDB_password = 'apPYSmCgu5m8O6u6'
+
+// connect to database
+mongoose.connect(`mongodb+srv://shankSauce2023:${mongoDB_password}@cluster0.sqeyv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
+    .then( () => {
+        console.log('Connected to database ');
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. \n${err}`);
+    });
+
 app.use(morgan('dev'))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
+
 
 
 
